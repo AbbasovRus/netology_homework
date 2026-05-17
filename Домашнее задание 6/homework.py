ec1c7aa1e569dcc89eb252cec6d7ceb110f45218
Abbasov Rustam <RustamAbbasov89@yandex.ru>
2026-05-17 22:45:49 +0300

    Удаление комментариев

--- Домашнее задание 6/homework.py
+++ Домашнее задание 6/homework.py
@@ -3,12 +3,11 @@ import json
 purchases = {}
 
 with open('purchase_log.txt', 'r', encoding='utf-8') as f:
-    next(f)  # Пропускаем первую строку (заголовок)
+    next(f) 
     for line in f:
         if line.strip():
             data = json.loads(line)
             purchases[data['user_id']] = data['category']
 
-# Вывод
 for user_id, category in list(purchases.items())[:20]:
     print(repr(user_id), repr(category))
